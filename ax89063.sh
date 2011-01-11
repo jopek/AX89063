2e7251923cb04913a208061f89322e48af50bc89
Malte S. Stretz <mss@apache.org>
2011-01-11 03:02:00 +0100

    increase log level

--- ax89063.sh
+++ ax89063.sh
@@ -12,6 +12,6 @@ set -e
 
 cd $(make getdir)
 
-(sleep 1; server/LCDd -f -c ./LCDd.conf -r 4 "$@") &
+(sleep 1; server/LCDd -f -c ./LCDd.conf -r 5 "$@") &
 socat PTY,link=./LCDd.tty,raw,echo=0,wait-slave -
 
